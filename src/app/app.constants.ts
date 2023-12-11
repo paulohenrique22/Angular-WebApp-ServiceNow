51fd803f4228dba4ed21e45cac609ee96393a26b
Paulo Henrique <52937187+paulohenrique22@users.noreply.github.com>
2023-12-11 13:59:44 -0300

    Update app.constants.ts

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,8 +1,9 @@
-export const SERVER_API_URL = 'https://dev89855.service-now.com/';
-export const USER_INFO_URL = '/api/x_19668_test/getuserdetails';
+export const SERVER_API_URL = 'https://dev178792.service-now.com/';
+// export const USER_INFO_URL = '/api/x_19668_test/getuserdetails';
 
-export const OAUTH2_CLIENT_ID= '3e3d87d601a501107d5aadce8576d935';
-export const OAUTH2_CLIENT_SECRET= 'F:rd{]0Sp&';
+export const USER_INFO_URL = '/api/349201/user_info';
+export const OAUTH2_CLIENT_ID= '71fe734df69173006fab82bf6fe27458';
+export const OAUTH2_CLIENT_SECRET= '*:eO3ez$0C';
 export const OAUTH2_ACCESS_TOKEN_URI = SERVER_API_URL + 'oauth_token.do';
 
 export const INCIDENT_API_URL = 'api/now/table/incident?sysparm_display_value=true';
